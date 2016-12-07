5eb507c80a6f345d48ccf4c334ea9de3a1cab2c0
Damien Tailhades <hello@regulardesigner.com>
2016-12-07 23:45:41 +0100

    add some Pilip magic code ;)

--- bot/js/regbot.js
+++ bot/js/regbot.js
@@ -1,45 +1,26 @@
 function hello(){
-	//alert(welcomeText);
-	var helloWorld = ("Hello, Bienvenu sur mon portfolio en ligne.");
-	botBubble(helloWorld, 2000);
+	var text = ("Bienvenu, je suis Reg-bot. <img src=\"img/1f601.png\"/ width=\"24\" align=\"bottom\">");
+	botBubble(text, 2000);
 	setTimeout(hello2, 5000);
 };
 
-function hello2 (){
-	var text = ("Vous avez la possibilité d'y découvrir mon <strong>parcoure</strong> et mon <strong>portfolio</strong>.")
+function hello2(){
+	var text = ("Damien m'a donné pour mission de vous accueillir sur son Portfolio.");
 	botBubble(text, 2500);
 	setTimeout(hello3, 5000);
 };
 
-function hello3 (){
-	var text = ("Cliquez simplement sur les bouton ci-dessous. <br> <br> <button onclick=\"parcoure()\">Parcoure</button><button onclick=\"portfolio()\">Portfolio</button>");
+function hello3(){
+	var text = ("Je suis là pour vous fournir des informations sur son travail de designer Ui/Ux et son parcours.");
 	botBubble(text, 5000);
+	setTimeout(hello4, 6000);
 };
 
-function botBubble(txt, time){
-	$('.buble').first().clone().appendTo('.conversation');
-	$('.conversation .buble').last().addClass('regbot');
-	$('.conversation .regbot span').last().html('<span class="talking"></span><span class="talking"></span><span class="talking"></span>').show();
-	scrollToBottom();
-	setTimeout(textBuble, time);
-	function textBuble (){$('.conversation .regbot span').not('.talking').last().html(txt).addClass('pop');
-		scrollToBottom();
-	};
-	
-};
-
-function scrollToBottom(){
-	//window.scrollTo(0,document.body.scrollHeight);
-	$(document).scrollTop($(document).height());
+function hello4(){
+	var text = ('Par quoi voulez-vous commencer ? <br><br> <button href="">Son Parcours</button> <button href="">Son Portfolio</button>');
+	botBubble(text, 2500);
 }
 
-function userBuble(txt, time){
-	$('.buble').first().clone().appendTo('.conversation');
-	$('.conversation .buble').last().addClass('user');
-	$('.conversation .user span').last().html('<span class="talking"></span><span class="talking"></span><span class="talking"></span>').show();
-	$('.conversation .user span').not('.talking').last().html(txt).addClass('pop');
-	scrollToBottom();
-};
 
 function portfolio(){
 	userBuble ('Portfolio', 0);
@@ -84,6 +65,31 @@ function non (){
 	else {
 		alert('Non');
 	}
+};
+
+function botBubble(txt, time){
+	$('.buble').first().clone().appendTo('.conversation');
+	$('.conversation .buble').last().addClass('regbot');
+	$('.conversation .regbot span').last().html('<span class="talking"></span><span class="talking"></span><span class="talking"></span>').show();
+	scrollToBottom();
+	setTimeout(textBuble, time);
+	function textBuble (){$('.conversation .regbot span').not('.talking').last().html(txt).addClass('pop');
+		scrollToBottom();
+	};
+	
+};
+
+function scrollToBottom(){
+	//window.scrollTo(0,document.body.scrollHeight);
+	$(document).scrollTop($(document).height());
+}
+
+function userBuble(txt, time){
+	$('.buble').first().clone().appendTo('.conversation');
+	$('.conversation .buble').last().addClass('user');
+	$('.conversation .user span').last().html('<span class="talking"></span><span class="talking"></span><span class="talking"></span>').show();
+	$('.conversation .user span').not('.talking').last().html(txt).addClass('pop');
+	scrollToBottom();
 };
 
 setTimeout(hello, 1500);
